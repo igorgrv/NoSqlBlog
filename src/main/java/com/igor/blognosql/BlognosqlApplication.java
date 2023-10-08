135770e6844d894ae58a1f4b71ee5b02d0404cd4
igorromero <igor.romero@ibm.com>
2023-10-08 18:18:49 -0300

    Adding basics

---
+++ src/main/java/com/igor/blognosql/BlognosqlApplication.java
@@ -0,0 +1,13 @@
+package com.igor.blognosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlognosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlognosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/blognosql/BlognosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.blognosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlognosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
